f8dfc8dc6b6c08d61f8b50dfda7bf953eee3e807
Victor <victorkiariem@gmail.com>
2022-07-05 12:27:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,29 @@
 
 /**
  * main-This Function prints _putchar using the local header file
- * Description: Writes a program that prints _putchar, followed by a new line.Return: 0 (Success)
+ *
+ * Return: nothing, return void
 */
 
 int main(void)
 {
-	char x[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	unsigned int i;
+_putchar('_');
 
-	for (i = 0; i < sizeof(x); i++)
-	{
-		_putchar(x[i]);
-	}
-	_putchar('\n');
+_putchar('p');
 
-	return (0);
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
+return (0);
 }
