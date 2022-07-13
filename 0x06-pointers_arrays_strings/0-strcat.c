1cb96aaeb8f5059da3c3e6ead604094c26f7f27e
Victor <victorkiariem@gmail.com>
2022-07-13 09:06:46 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 *_strcat - concatenates two strings
 *@dest: A pointer to a character that will be changed
@@ -10,12 +9,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+
 	j = 0;
 	while (src[j] != '\0')
 	{
@@ -24,5 +24,6 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 *_strncat - concatenates two strings
 *@dest: A pointer to a character that will be changed
@@ -9,14 +8,16 @@
 */
 
 char *_strncat(char *dest, char *src, int n)
+
 {
 	int i, j;
+	
 	i = 0;
-
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
